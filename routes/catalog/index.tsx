da21c54c42116001af2f9666e5acef095f4a1e8f
hashrock <hashrock@users.noreply.github.com>
2023-11-25 02:34:15 +0900

    nemui

--- routes/catalog/index.tsx
+++ routes/catalog/index.tsx
@@ -4,10 +4,12 @@ import { defineRoute } from "$fresh/server.ts";
 import { expandGlob } from "https://deno.land/std@0.208.0/fs/expand_glob.ts";
 
 function toRelativePath(path: string) {
-  return path.replace(Deno.cwd(), "").replace(/^\//, "")
+  return path.replace(Deno.cwd(), "").replace(/^\//, "");
 }
 
 export default defineRoute(async (req, ctx) => {
+  const path = ctx.url.searchParams.get("path");
+
   const stories = [
     "Button.stories",
   ];
@@ -16,13 +18,18 @@ export default defineRoute(async (req, ctx) => {
     <main>
       <h1>Index</h1>
 
-      <ul>
-        {stories.map((story) => (
-          <li>
-            <a href={`/catalog/${story}`}>{story}</a>
-          </li>
-        ))}
-      </ul>
+      <div class="grid grid-cols-2">
+        <ul>
+          {stories.map((story) => (
+            <li>
+              <a href={`?path=catalog/${story}`}>{story}</a>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <iframe src={`./${path}`} />
+        </div>
+      </div>
     </main>
   );
 });
