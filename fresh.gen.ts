2c5f0d23e6d2723062c5cbb1ff35cec91916c6ce
hashrock <hashrock@users.noreply.github.com>
2023-11-27 19:12:36 +0900

    rename

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,8 +4,8 @@
 
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/catalog.tsx";
-import * as $3 from "./routes/index.tsx";
+import * as $2 from "./routes/index.tsx";
+import * as $3 from "./routes/stories.tsx";
 import * as $$0 from "./islands/ColoredButton.tsx";
 import * as $$1 from "./islands/Counter.tsx";
 import * as $$2 from "./islands/StoryFrame.tsx";
@@ -20,8 +20,8 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_app.tsx": $1,
-    "./routes/catalog.tsx": $2,
-    "./routes/index.tsx": $3,
+    "./routes/index.tsx": $2,
+    "./routes/stories.tsx": $3,
   },
   islands: {
     "./islands/ColoredButton.tsx": $$0,
