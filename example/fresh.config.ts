68015c9c78b05875ed89056358e4ce4e92128cb2
hashrock <hashrock@users.noreply.github.com>
2023-12-03 23:05:27 +0900

    fix ext

--- example/fresh.config.ts
+++ example/fresh.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
-import storiesPlugin from "../stories-plugin.tsx";
+import storiesPlugin from "../stories-plugin.ts";
 
 export default defineConfig({
   plugins: [tailwind(), storiesPlugin()],
