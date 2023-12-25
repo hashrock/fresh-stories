6bd07c678b0318b031348e7dc2d0b751b664905d
hashrock <hashrock@users.noreply.github.com>
2023-12-25 21:56:29 +0900

    comment out copyable

--- www/islands/stories/Hero.story.tsx
+++ www/islands/stories/Hero.story.tsx
@@ -1,5 +1,5 @@
 import Hero from "../../components/Hero.tsx";
-import Copyable from "https://deno.land/x/fresh_stories@0.0.11/islands/Copyable.tsx";
+// import Copyable from "https://deno.land/x/fresh_stories@0.0.11/islands/Copyable.tsx";
 export default function Stories() {
   return <Hero />;
 }
@@ -9,12 +9,14 @@ export const description = (
     <p>
       You can also write JSX as a description.
     </p>
-    <Copyable
+    {
+      /* <Copyable
       code={`<button
   {...props}
   disabled={!IS_BROWSER || props.disabled}
   class="px-3 py-2 bg-blue-200 text-blue-800 rounded hover:bg-blue-300 active:bg-blue-400"
 />`}
-    />
+    /> */
+    }
   </>
 );
